ea1bff1ed01a5e8c031336a93e08157ccebf2310
Miroslava Kostova <mirajka0512@gmail.com>
2025-01-25 15:02:23 +0200

    Added project R-P-S

---
+++ rock_paper_scissors.py
@@ -0,0 +1,29 @@
+import random
+
+choice_list = ["rock", "paper", "scissors"]
+computer_choice = random.choice(choice_list)
+
+while True:
+    player_choice = input("Pick your fighter! ")
+
+    if computer_choice == player_choice:
+        print("It's a tie!")
+        break
+    if computer_choice == "rock" and player_choice == "paper":
+        print("You win!")
+        break
+    if computer_choice == "rock" and player_choice == "scissors":
+        print("Ha, I win!")
+        break
+    if computer_choice == "scissors" and player_choice == "rock":
+        print("You win!")
+        break
+    if computer_choice == "scissors" and player_choice == "paper":
+        print("Ha, I win!")
+        break
+    if computer_choice == "paper" and player_choice == "scissors":
+        print("You win!")
+        break
+    if computer_choice == "paper" and player_choice == "rock":
+        print("Ha, I win!")
+        break
