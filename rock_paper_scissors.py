f65d70ce79fc078f1b4b73da40939f245074aebf
Miroslava Kostova <mirajka0512@gmail.com>
2025-01-25 15:18:43 +0200

    Update R-P-S

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,23 +7,30 @@ while True:
     player_choice = input("Pick your fighter! ")
 
     if computer_choice == player_choice:
+        print(f"I choose {computer_choice}.")
         print("It's a tie!")
         break
     if computer_choice == "rock" and player_choice == "paper":
+        print(f"I choose {computer_choice}.")
         print("You win!")
         break
     if computer_choice == "rock" and player_choice == "scissors":
+        print(f"I choose {computer_choice}.")
         print("Ha, I win!")
         break
     if computer_choice == "scissors" and player_choice == "rock":
+        print(f"I choose {computer_choice}.")
         print("You win!")
         break
     if computer_choice == "scissors" and player_choice == "paper":
+        print(f"I choose {computer_choice}.")
         print("Ha, I win!")
         break
     if computer_choice == "paper" and player_choice == "scissors":
+        print(f"I choose {computer_choice}.")
         print("You win!")
         break
     if computer_choice == "paper" and player_choice == "rock":
+        print(f"I choose {computer_choice}.")
         print("Ha, I win!")
         break
